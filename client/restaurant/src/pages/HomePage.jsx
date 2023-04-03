354ee8f6a90644708df5a5f3e2547ee443b3f7a9
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-04-03 15:09:36 +0200

    added login button and form, Johan and Admir

---
+++ client/restaurant/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import LogInButton from "../components/LogInButton.jsx";
+
+export default function() {
+    return (
+        <main className='h-screen flex justify-center items-center'>
+        < LogInButton />
+        </main>
+    )
+}
