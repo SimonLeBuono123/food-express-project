bd12a5c7d51960ed6bf21f94ef6fc2caa21b7607
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-04-03 14:40:56 +0200

    added login-routes, Admir and Johan

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const api = express();
 const port = 3030;
 const uri = `mongodb+srv://food-project:${password}@food-cluster0.a9dyz1j.mongodb.net/?retryWrites=true&w=majority`;
 
+import session from 'express-session'
+
+// när session körs så kommer den att returnera en funktion som har request response och next
+api.use(session({
+  secret: 'keyboard cat sdovsdjn', //hash för att hemlighålla databasen även om någon stjäl den long random string
+  resave: false, //only saved if modified
+  saveUninitialized: true, //session och cookie (create session cookie) skapas altid även om användaren inte gjort något
+  cookie: { secure: false, maxAge: 365 * 24 * 60 * 60 * 1000, httpOnly: true } //https, ska vara på i produktion men ej under utveckling, använd envirement variabel i riktig produktion
+  //httpOnly: true, cookien är inte tillgänglig i Javascript scopet, ingen kan stjäla sessionen om man har kommit över webbläsaren
+}))
+
 api.use(function(req, res, next){
   res.set({
     "Content-Type": "application/json",
--- server/routes/restaurant-login-routes.js
+++ server/routes/restaurant-login-routes.js
@@ -1,55 +1,55 @@
 import Router from "express";
+
 const restaurantLoginRouter = Router()
 
 //crypto
-import encrypt from "../utilities/encrypt";
+import encrypt from "../utilities/encrypt.js";
 
 
 import mongoose from "mongoose";
 
-restaurantLoginRouter.post('/', async (request, response)=>{
-let employee = await mongoose.models.employees.findOne({
-email: request.body.email,
-password: encrypt(request.body.password)
+restaurantLoginRouter.post('/', async (request, response) => {
+    let employee = await mongoose.models.employees.findOne({
+        email: request.body.email,
+        password: encrypt(request.body.password)
+    })
+
+    if (employee) {
+        response.status(200)
+        request.session.employee = employee
+        response.json({loggedIn: true})
+    } else {
+        response.status(401)
+        response.json({loggedIn: false})
+    }
 })
 
-if (employee){
-response.status(201)
-request.session.employee = employee
-response.json({loggedIn: true})
-} else {
-response.status(401)
-response.json({loggedIn: false})
-}
-})
-
-restaurantLoginRouter.get('/', async (request, response)=>{
-if (request.session?.employee){
+restaurantLoginRouter.get('/', async (request, response) => {
+    if (request.session?.employee) {
 //first check with database
-let employee = await mongoose.models.employees.findOne({
-email: request.session.employee.email,
-password: request.session.employee.password
-})
-if (employee){
+        let employee = await mongoose.models.employees.findOne({
+            email: request.session.employee.email,
+            password: request.session.employee.password
+        })
+        if (employee) {
 //respond
-response.status(401)
-response.json({
-name: request.session.employee.name,
-email: request.session.employee.email,
-loggedIn: true
-})
-return
-}
-}
-response.status(401)
-response.json({loggedIn: false})
+            response.status(401)
+            response.json({
+                email: request.session.employee.email,
+                loggedIn: true
+            })
+            return
+        }
+    }
+    response.status(401)
+    response.json({loggedIn: false})
 })
 
-restaurantLoginRouter.delete('/', async (request, response)=>{
+restaurantLoginRouter.delete('/', async (request, response) => {
 
-delete(request.session.employee)
-response.status(401)
-response.json({loggedIn: false})
+    delete (request.session.employee)
+    response.status(401)
+    response.json({loggedIn: false})
 })
 
 export default restaurantLoginRouter
