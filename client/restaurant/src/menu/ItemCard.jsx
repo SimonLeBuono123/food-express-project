36f03bd02107d8b10efdb1c9dad01cc387f67f06
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2023-04-05 09:29:37 +0200

    connected item card to menu page
    Johan and Admir

--- client/restaurant/src/menu/ItemCard.jsx
+++ client/restaurant/src/menu/ItemCard.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 
 
-function MenuCard({details}) {
+export default function({types}) {
 
-    const { name, ingredients, categories, price} = details
-    return (
+console.log(types)
+
+    const listItems = types.map((type)=>{
+        return (
+            <li key={type._id} className='flex items-center justify-between m-5'>
+                <div>
+                    <h2>{type.name}</h2>
+                </div>
+            </li>
+        )
+    })
+console.log(listItems)
+    return <>
+        <h3>
+            {types.categories}
+        </h3>
+        <ul>
+            {listItems}
+        </ul>
+    </>
+       /*
         <div className="bg-white w-64 h-32 flex flexbox flex-col rounded-xl shadow-lg pr-3 justify-center item-center">
 
-            <h3 className="text-center underline" >{name}</h3>
+            <h3 className="text-center underline">{name}</h3>
             <p>Ingredients: {ingredients}</p>
             <p className="text-end">{categories.name}</p>
             <p className="text-end">{price}</p>
         </div>
-    );
-}
+        */
 
-export default MenuCard;
+
+}
--- client/restaurant/src/menu/MenuPage.jsx
+++ client/restaurant/src/menu/MenuPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext, useEffect } from "react";
 import globalContext from "../globalContext.jsx";
+import ItemCard from "./ItemCard.jsx";
 
 export default function () {
   const { items } = useContext(globalContext);
@@ -7,16 +8,27 @@ export default function () {
 
   useEffect(() => {
     setAllItems(items);
-  }, [items]);
+  }, [...items]);
 
-  console.log(allItems)
+console.log(allItems)
+    const mains = items.filter(item => item.categories.name === "Main")
+    const sides = items.filter(item => item.categories.name === "Side")
+    const drinks = items.filter(item => item.categories.name === "Drink")
+
   return (
-    <section className={"h-screen w-screen flex"}>
+    <section className={"h-screen w-screen flex justify-center m-5"}>
+        <div className='flex flex-col'>
       <h2>Main</h2>
-
-      <h2>Condiments</h2>
-
-      <h2>Drinks</h2>
+        < ItemCard types={mains}/>
+        </div>
+        <div className='flex flex-col'>
+        <h2>Condiments</h2>
+        < ItemCard types={sides}/>
+        </div>
+        <div className='flex flex-col'>
+        <h2>Drinks</h2>
+        <ItemCard types={drinks}/>
+        </div>
     </section>
   );
 }
