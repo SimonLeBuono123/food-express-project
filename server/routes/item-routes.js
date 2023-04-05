a63bf6bd773ea502b9f0a40c76d0cf33104f9802
jm <johan.moller040@gmail.com>
2023-04-05 15:33:04 +0200

    added admin permission for delete, johan, admir

--- server/routes/item-routes.js
+++ server/routes/item-routes.js
@@ -1,60 +1,55 @@
 import Router from "express";
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import restaurantLoginRouter from "./restaurant-login-routes.js";
-
 
 const itemRoutes = Router();
 
+const itemSchema = new Schema({
+  name: String,
+  ingredients: String,
+  categories: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
+  price: Number,
+});
 
+mongoose.model("items", itemSchema);
 
-const itemSchema = new Schema(
-    {
-        name: String,
-        ingredients: String,
-        categories: {type: mongoose.Schema.Types.ObjectId, ref: "categories" },
-        price: Number
-    }
-)
-
-mongoose.model('items', itemSchema)
-
-
-itemRoutes.post('/', async(request, response)=> {
-    const {name, ingredients, categories, price} = request.body
-    try {
+itemRoutes.post("/", async (request, response) => {
+  const { name, ingredients, categories, price } = request.body;
+  try {
     const newItem = new mongoose.models.items({
-        name,
-        ingredients,
-        categories,
-        price
-    })
-    await newItem.save()
-        response.json(newItem)
-    }catch(error){
-        console.error(error)
+      name,
+      ingredients,
+      categories,
+      price,
+    });
+    await newItem.save();
+    response.json(newItem);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+itemRoutes.get("/", async (request, response) => {
+  const item = await mongoose.models.items.find().populate("categories").exec();
+  response.json(item);
+});
+
+itemRoutes.delete("/:id", async (request, response) => {
+  try {
+    if (request.session?.employee.admin) {
+      const item = await mongoose.models.items.findByIdAndDelete(
+        request.params.id
+      );
+
+      response.status(200);
+      response.json(item);
+    } else {
+      response.status(401).json("Unauthorized");
     }
+  } catch (error) {
+    console.error(error);
+    response.status(404).json("not found");
+  }
+});
 
-})
-
-
-itemRoutes.get('/', async (request, response) => {
-    const item = await mongoose.models.items.find().populate('categories').exec()
-    response.json(item)
-})
-
-
-itemRoutes.delete('/:id', async (request, response) => {
-    try {
-        const item = await mongoose.models.items.findByIdAndDelete(request.params.id)
-
-        response.status(200)
-        response.json(item)
-    }catch(error){
-            console.error(error)
-            response.status(404).json('not found')
-        }
-})
-
-
-
-export default itemRoutes
+export default itemRoutes;
