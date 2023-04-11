8ddb916d33d6c8387e25e9e712afc9002bbff18f
jm <johan.moller040@gmail.com>
2023-04-11 15:45:12 +0200

    polishing price and date functions, johan, admir

--- client/restaurant/src/orders/OrderCard.jsx
+++ client/restaurant/src/orders/OrderCard.jsx
@@ -9,11 +9,6 @@ export default function ({ allOrders, setAllOrders }) {
     setEstimatedTime(estimatedTime + 15);
   }
 
-  function calculatePickupDate() {
-      return new Date(Date.now() + estimatedTime * 60 * 1000)
-  }
-
-
   function confirmOrder(event, id) {
     let newIsAccepted = true;
     let newIsDelivered = false;
@@ -21,11 +16,12 @@ export default function ({ allOrders, setAllOrders }) {
     let newTotalPrice = calculateTotalPrice(id);
     let updatedOrders = allOrders.map((order) => {
       if (order._id === id) {
-        return { ...order, 
-            isAccepted: newIsAccepted,
-            isDelivered: newIsDelivered,
-            pickupDate: newPickupDate,
-            totalPrice: newTotalPrice
+        return {
+          ...order,
+          isAccepted: newIsAccepted,
+          isDelivered: newIsDelivered,
+          pickupDate: newPickupDate,
+          totalPrice: newTotalPrice,
         };
       }
 
@@ -34,8 +30,46 @@ export default function ({ allOrders, setAllOrders }) {
     setAllOrders(updatedOrders);
     patchOrder(id, newPickupDate, newIsDelivered, newIsAccepted, newTotalPrice);
 
-
     setEstimatedTime(0);
+  }
+
+  function calculateTotalPrice(_id) {
+    let result = 0;
+    allOrders.map((order) => {
+      if (order._id === _id) {
+        order.items.map((item) => {
+          result = result + item.price;
+        });
+      }
+    });
+    return result;
+  }
+
+  function calculatePickupDate() {
+    return new Date(Date.now() + estimatedTime * 60 * 1000);
+  }
+
+  function formatOrderDate(date) {
+    let orderDate = new Date(date);
+    let year = orderDate.getFullYear();
+    let month = orderDate.getMonth() + 1; // Add 1 to get the month in human-readable format (0-indexed)
+    let day = orderDate.getDate();
+
+    return year.toString() + "/" + month.toString() + "/" + day.toString();
+  }
+
+  function handleOrderStatus(order) {
+    if (order.isDelivered) {
+      return `Order delivered`;
+    }
+
+    if (!order.isAccepted) {
+      return "Waiting for confirmation";
+    } else if (order.isAccepted) {
+
+      return `Order confirmed, estimated delivery: ${Math.round(
+        (new Date(order.pickupDate) - Date.now()) / (1000 * 60))} min`;
+    }
   }
 
   allOrders.sort((a, b) => {
@@ -43,20 +77,6 @@ export default function ({ allOrders, setAllOrders }) {
     const dateB = new Date(b.orderDate);
     return dateA - dateB;
   });
-
-  function calculateTotalPrice(_id) {
-      let result = 0
-allOrders.map(order => {
-    if (order._id === _id) {
-    order.items.map(item => {
-        result = result + item.price
-    })
-
-}
-
-})
-      return result
-  }
 
   let ordersList = allOrders.map((order) => {
     let itemsList = order.items.map((item) => {
@@ -87,13 +107,16 @@ allOrders.map(order => {
             Confirm
           </button>
         </div>
-        <p className="text-gray-500 text-sm mb-2">{`order wait: ${Math.floor(
-          (Date.now() - new Date(order.orderDate)) / (1000 * 60)
+        <p className="text-gray-600 text-sm">
+          {formatOrderDate(order.placedDate)}
+        </p>
+        <p className="text-gray-500 text-sm mb-2">{`Wait time: ${Math.round(
+          (Date.now() - new Date(order.placedDate)) / (1000 * 60)
         )} min`}</p>
         <ul className="pl-4 mb-4">{itemsList}</ul>
-        <p>{order.totalPrice} SEK</p>
-        <p className="text-gray-600 text-sm">{order.isDelivered}</p>
+        {(order.isAccepted) ? <p>{order.totalPrice.toString()} SEK</p> : null}
 
+        <p>{"Status: " + handleOrderStatus(order)}</p>
       </li>
     );
   });
