9764ef962e3c25464beff02e7ce3581d02000711
jm <johan.moller040@gmail.com>
2023-04-13 17:52:49 +0200

    removed testRoutes

--- server/index.js
+++ server/index.js
@@ -43,9 +43,6 @@ api.listen(port, () => {
   });
 });
 
-import testRouter from "./routes/test-routes.js";
-api.use("/rest/test", testRouter);
-
 import customersRouter from "./routes/customers-routes.js";
 api.use("/rest/customers", customersRouter);
 
